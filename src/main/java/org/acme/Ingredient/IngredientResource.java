97ddb1bf4fa3a36f23dce6a5fe3b2c7866cbd12c
Daniel Roithmeier <roithmeierda@gmail.com>
2024-04-14 01:12:38 +0200

    renamed amount to ingredient

--- src/main/java/org/acme/Ingredient/IngredientResource.java
+++ src/main/java/org/acme/Ingredient/IngredientResource.java
@@ -1,9 +1,9 @@
-package org.acme.Amount;
+package org.acme.Ingredient;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class AmountResource implements PanacheRepository<Amount> {
+public class IngredientResource implements PanacheRepository<Ingredient> {
 }
